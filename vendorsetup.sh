78ee66da7c94169e8740058f68fa13db25b358a1
Chronix <phoniexpaul88@gmail.com>
2025-09-08 18:38:02 +0530

    Common Trees For Building

---
+++ vendorsetup.sh
@@ -0,0 +1,7 @@
+#Device Tree
+git clone https://github.com/xiaomi-sm6150/android_device_xiaomi_sm6150-common.git device/xiaomi/sm6150-common
+#Vendor Tree
+git clone https://github.com/xiaomi-sm6150/proprietary_vendor_xiaomi_violet.git vendor/xiaomi/violet
+git clone https://github.com/xiaomi-sm6150/proprietary_vendor_xiaomi_sm6150-common.git vendor/xiaomi/sm6150-common
+#Kernel
+git clone https://github.com/xiaomi-sm6150/android_kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150-common
